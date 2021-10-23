097a0ce8be821ba5a566ec1680e2a85b462fceae
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2021-10-23 13:07:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const linksSocialMedia = {
+  github: 'francisco-das-chagas',
+  instagram: 'jakelinygracielly',
+  youtube: 'maykbrito',
+  facebook: 'jakeliny.gracielly',
+  twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+// Incrementar:
+// i = i + 1
+// i++
+
+// camelCase
+// PascalCase
+// snake_case
+
+// Pegar e Trazer informações da API
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
+
+// ARROW FUNCTIONS
+// function nomedafuncao(argumentos) {
+//   codigo
+// }
